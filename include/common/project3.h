66619b1b7ba8d65be503844f7641ead720fc4443
Yue Wei <yuwe8903@colorado.edu>
2018-04-08 11:57:29 -0600

    added project3.h

---
+++ include/common/project3.h
@@ -0,0 +1,25 @@
+
+/***************************************************************
+*  @file project3.h
+*  @description declare macros and functions for project3.c
+*  @author Yue Wei
+*  @date  Apr 7, 2018
+****************************************************************/
+#ifndef _PROJECT3_H_
+#define _PROJECT3_H_
+
+#include<stdint.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+#include "memory.h"
+#include "profiling.h"
+
+#include "dma.h"
+#include "spi.h"
+#include "nordic.h"
+#include "gpio.h"
+void spi_nrf_interface();
+void profile_demo();
+
+#endif /* _PROJECT3_H_ */
